1850319c9aa2ceeccbe6c270e2c91647e013648a
Serhii Chaban <serhii@scanbot.io>
2023-09-08 15:22:38 +0200

    code cleanup add comments

--- app/src/main/java/com/example/scanbot/ExampleApplication.kt
+++ app/src/main/java/com/example/scanbot/ExampleApplication.kt
@@ -1,6 +1,9 @@
 package com.example.scanbot
 
 import android.app.Application
+import android.widget.Toast
+import io.scanbot.sap.IScanbotSDKLicenseErrorHandler
+import io.scanbot.sap.Status
 import io.scanbot.sdk.ScanbotSDKInitializer
 import io.scanbot.sdk.core.contourdetector.ContourDetector
 import io.scanbot.sdk.persistence.CameraImageFormat
@@ -10,6 +13,14 @@ import io.scanbot.sdk.process.ImageProcessor
 class ExampleApplication : Application() {
 
     companion object {
+        /**
+         * TODO Add the Scanbot Document Scanner SDK license key here.
+         * Please note: Scanbot Document Scanner SDK will run without a license key for one minute per session!
+         * After the trial period has expired all SDK features and UI components will stop working.
+         * You can get a free "no-strings-attached" trial license key. Please submit the trial license
+         * form (https://scanbot.io/trial) on our website by using the app identifier
+         * "io.scanbot.example.document.usecases.android" of this example app.
+         */
         private const val LICENSE_KEY = "" // "YOUR_SCANBOT_SDK_LICENSE_KEY"
         const val USE_ENCRYPTION = false
     }
@@ -27,6 +38,26 @@ class ExampleApplication : Application() {
                     .imageQuality(80)
                     .build()
             )
+            .licenceErrorHandler(IScanbotSDKLicenseErrorHandler { status, sdkFeature, s ->
+                when (status) {
+                    Status.StatusFailureNotSet,
+                    Status.StatusFailureCorrupted,
+                    Status.StatusFailureWrongOS,
+                    Status.StatusFailureAppIDMismatch,
+                    Status.StatusFailureExpired -> {
+                        Toast.makeText(
+                            this,
+                            "License error: $status ",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                    else -> {
+
+                    }
+                }
+
+            })
             .useFileEncryption(USE_ENCRYPTION)
             .imageProcessorType(ImageProcessor.Type.ML_BASED)
             .license(this, LICENSE_KEY)
